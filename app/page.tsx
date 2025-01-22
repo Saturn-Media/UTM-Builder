df43e74bf0e15f08ddab84af710fd9e8c94a5a0e
Saturn-Media <Ciaran@saturn-media.co.uk>
2025-01-22 23:30:45 +0200

    app/page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import UTMBuilder from '@/components/utm-builder'
+
+export default function Home() {
+  return (
+    <main className="min-h-screen bg-gradient-to-b from-black to-zinc-900 flex items-center justify-center p-4">
+      <UTMBuilder />
+    </main>
+  )
+}
